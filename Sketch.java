1612d06a9e887b94ecfd0c31da93be6516b4672c
danielpetralito <99832159+Dpetralito@users.noreply.github.com>
2022-04-18 18:36:03 +0000

    doNE

--- Sketch.java
+++ Sketch.java
@@ -1,35 +1,64 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+	// images used
+  PImage imgBeach;
+  PImage imgDino;
+  PImage imgNuke;
+
+  float fltBeachX = 0;
+  float fltBeachY = 0;
+
+  float fltDinoX = 0;
+  float fltDinoY = 300;
+
+  float fltNukeX = 0;
+  float fltNukeY = 0;
+
+  float fltDinoSpeedX = 3;
+
+  float fltNukeSpeedX = 4;
+  float fltNukeWaveX;
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    
+    background(255);
+    
+    imgBeach = loadImage("beach.jpg"); 
+    imgDino = loadImage("dinosaur.png");
+    imgNuke = loadImage("nuke.png");
+    
+    imgDino.resize(imgDino.width / 7, imgDino.height / 7);
+    imgNuke.resize(imgNuke.width / 8, imgNuke.height / 8);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    image(imgBeach, fltBeachX, fltBeachY);
+    image(imgDino, fltDinoX, fltDinoY);
+    
+    fltDinoX += fltDinoSpeedX;
+
+    if (fltDinoX < 0 || fltDinoX > width - 120) {
+    fltDinoSpeedX *= -1;
+  }
+        
+    image(imgNuke, fltNukeX, fltNukeY);
+
+    fltNukeX += fltNukeSpeedX;
+    if (fltNukeX < 0 || fltNukeX > width - 60) {
+    fltNukeSpeedX *= -1;
+    }
+
+    fltNukeY = ((float) 0.006 * ((float) Math.pow(fltNukeX - width / (float) 2, (float) 2))) + (float) 10;
+    fltNukeWaveX = width - (float) fltNukeX;
+    
   }
   
   // define other methods down here.
